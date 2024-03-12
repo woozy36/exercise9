bf43e14c1db38a8aee01ec62c86adae2b3c74226
117090 <117090@swcsd.us>
2024-03-12 13:26:13 -0400

    gitpart2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.concurrent.Phaser;
+
 public class Main {
     public static void main(String[] args) {
 
+        Email email1 = new Email("dumbhead@dumbemail.com");
+        PhoneNumber number1 = new PhoneNumber("dumbhead@dumbemail.com");
 
-
+        email1.contact();
+        number1.contact();
 
 
     }
@@ -10,4 +15,34 @@ public class Main {
 
 abstract class Contacts {
 
+    public abstract void contact();
+
+}
+
+class Email extends Contacts{
+
+    private String email;
+
+    public Email(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void contact() {
+        System.out.println("EMAIL " + email);
+    }
+}
+
+class PhoneNumber extends Contacts{
+
+    private String phoneNumber;
+
+    public PhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public void contact() {
+        System.out.println("PHONE NUMBER " + phoneNumber);
+    }
 }
